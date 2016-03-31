cad209e64b07b8cf07810269277f112214405bc5
John Harnett <John.Harnett@gmail.com>
2016-03-31 17:52:26 +0100

    take in product json convert to xml write xml to file

--- product_create.rb
+++ product_create.rb
@@ -1,50 +1,60 @@
 require 'sinatra'
 require 'sinatra/config_file'
 require 'nokogiri'
+require 'json'
 
 set :lock, true
 set :raise_errors, true
 
 config_file './config/config.yml'
 
-def create_product sku
+def create_product product
   builder = Nokogiri::XML::Builder.new do |xml|
-    xml.Company {
-	    xml.Products {
-	      xml.Product  {
-		 xml.Sku sku.to_s
-		 xml.Name "Awesome widget"
-		 xml.Description "101.00"
-		 xml.LongDescription "this is a test"
-		 xml.SalePrice "101"
-		 xml.Custom1 "201.00"
-		 xml.Custom2 "221.00"
-		 xml.Custom3 "223.00"
-	      }  
-            }  
-          }
-      end
-      builder.to_xml
+    xml.Company('xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance','xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema') {
+      xml.Products {
+      xml.Product {
+            product.keys.each do |key|
+                  xml.send(key,product[key])
+            end
+             }
+         }
+     }
+  end
+  builder.to_xml
 end
 
+before do
+    request.body.rewind
+    @request_payload = JSON.parse request.body.read
+end
 
 post '/' do
 
-  the_product_xml = create_product "test_127"
-  
+  the_product_xml = create_product  @request_payload
+   
+
+  # File.open("#{settings.workflow_path}","w") do |f|
+  #  contents = f.read
+  #  parse contents
+  #  update contents to put in workflow variables such as file name
+  # f.write contents
+  # end
 
   File.open("c:\\temp\\test_127_stock_record_min.xml","w") do |f| #  puts "writing file"
     f.write the_product_xml
   end
 
   x = `"#{settings.app_path}" "/r" "#{settings.workflow_path}"` 
-  'success'
+  # wait for  update workflow to finish
+  # parse x? to generate status code?
+  201
 
 end
  
-get '/' do
- 'hello work worlds'
-end
  
+post '/test' do
+   p create_product @request_payload
+   201
+end
 
 
